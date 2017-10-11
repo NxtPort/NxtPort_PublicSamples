b048159f95ad29e9f3a3390a8145f7f6ed535dcf
DESKTOP-8MALLD1\kurtc <kurt@nuboid.com>
2017-10-11 16:08:41 +0200

    8

---
+++ NxtPort_PublicSamples_solution/NxtPort.Sample.MVC.CallApiHybridFlow/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace NxtPort.Sample.MVC.CallApiHybridFlow
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ NxtPort_PublicSamples_solution/NxtPort.Sample.MVC.CallApiHybridFlow/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NxtPort.Sample.MVC.CallApiHybridFlow.MvcApplication" Language="C#" %>
