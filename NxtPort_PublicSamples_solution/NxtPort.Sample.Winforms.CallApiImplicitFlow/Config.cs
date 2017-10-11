d7ecbcbb717e30ea9d22858661bd5d76a599e0e1
DESKTOP-8MALLD1\kurtc <kurt@nuboid.com>
2017-10-11 15:44:26 +0200

    6

--- NxtPort_PublicSamples_solution/NxtPort.Sample.Winforms.CallApiImplicitFlow/Config.cs
+++ NxtPort_PublicSamples_solution/NxtPort.Sample.Winforms.CallApiImplicitFlow/Config.cs
@@ -7,19 +7,19 @@ namespace NxtPort.Sample.Winforms.CallApiImplicitFlow
         //Replace with settings.
 
         //Urls
-        public const String STSURL = "<REPLACE>";
-        public const String APIURL = "<REPLACE>";
+        public const String STSURL = "REPLACE";
+        public const String APIURL = "REPLACE";
         
         //User
-        public const String DEMOUSER_USERNAME = "<REPLACE>";
-        public const String DEMOUSER_PASSWORD = "<REPLACE>";
+        public const String DEMOUSER_USERNAME = "REPLACE";
+        public const String DEMOUSER_PASSWORD = "REPLACE";
 
         //ApiKey
-        public const String APIKEY = "<REPLACE>";
+        public const String APIKEY = "REPLACE";
 
         //oAuth settings
-        public const String CLIENTID = "<REPLACE>";
-        public const String SCOPE = "<REPLACE>";
-        public const String REDIRECTURI = "<REPLACE>";
+        public const String CLIENTID = "REPLACE";
+        public const String SCOPE = "REPLACE";
+        public const String REDIRECTURI = "REPLACE";
     }
 }
--- NxtPort_PublicSamples_solution/NxtPort.Sample.Winforms.CallApiImplicitFlow/Form1.cs
+++ NxtPort_PublicSamples_solution/NxtPort.Sample.Winforms.CallApiImplicitFlow/Form1.cs
@@ -17,14 +17,14 @@ namespace NxtPort.Sample.Winforms.CallApiImplicitFlow
 
         private void btStart_Click(object sender, EventArgs e)
         {
+            //Start browser
             webBrowser.Visible = true;
 
             //First make sure the session to STS is ended and that we are logged out.
             webBrowser.Url = new Uri("https://login-dev.nxtport.eu/connect/endsession");
             webBrowser.Url = new Uri("https://login-dev.nxtport.eu/logout");
 
-            //Make the URL based on STS url, clientid, scope and redirecturi.
-            //You can change the values in the Config class. Simply copy paste the from the NxtPort MarketPlace portal.
+            //Make the URL based on STS url, clientid, scope and redirecturl.
             var template = "{0}/connect/authorize?client_id={1}&response_type=token&scope={2}&redirect_uri={3}";
             var url = string.Format(template, Config.STSURL, Config.CLIENTID, Config.SCOPE, Config.REDIRECTURI);
 
@@ -46,13 +46,15 @@ namespace NxtPort.Sample.Winforms.CallApiImplicitFlow
 
         private void webBrowser_Navigated(object sender, WebBrowserNavigatedEventArgs e)
         {
-            //After entering correct credentials teh browser redirects.
-            if (e.Url.ToString().StartsWith("http://localhost/test.redirect#"))
+            //After entering correct credentials the browser redirects to the redirecturl.
+            if (e.Url.ToString().StartsWith(Config.REDIRECTURI))
             {
-                var dict = e.Url.ToString().Replace("http://localhost/test.redirect#", "").Replace("?", "").Split('&')
+                var dict = e.Url.ToString().Replace(Config.REDIRECTURI + "#", "").Replace("?", "").Split('&')
                     .ToDictionary(x => x.Split('=')[0], x => x.Split('=')[1]);
+
                 //You can find the access token as a value in the querystring of this redirect.
                 _accessToken = dict["access_token"];
+                
                 //We got accesstoken now. Browser session is not needed anymore.
                 webBrowser.Visible = false;
 
@@ -64,40 +66,45 @@ namespace NxtPort.Sample.Winforms.CallApiImplicitFlow
         {
             try
             {
+                //Busy.
                 lbStatus.Text = @"Call in progress...";
 
-                //Create an http Client 
+                //Create an http Client. 
                 var client = new HttpClient();
-                
-                //Add the APIkey as Ocp-Apim-Subscription-Key header
+
+                //Add the APIkey as Ocp-Apim-Subscription-Key header.
                 client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", Config.APIKEY);
+
+                //Set accept header to receive json.
                 client.DefaultRequestHeaders.Add("Accept", "application/json");
-                
-                //Add the Accesstoken as Authorization header
+
+                //Add the Accesstoken as Authorization header.
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _accessToken);
-                
+
                 //Call the Get operation.
                 var response = client.GetAsync(Config.APIURL + "/vgm/v1/info?bn=33314669&cn=gesu1070100").Result;
-                
-                //Ensure success
+
+                //Ensure success.
                 response.EnsureSuccessStatusCode();
-                
-                //read the result
+
+                //Read the result.
                 var result = response.Content.ReadAsStringAsync().Result;
-                
-                //consume the result
+
+                //Consume the result.
                 tbResult.Text = result;
 
-                //Happy
+                //Happy ;)
                 lbStatus.Text = @"Done!";
 
             }
             catch (HttpRequestException exception)
             {
+                //Specific Oops
                 tbResult.Text = exception.ToString();
             }
             catch (Exception exception)
             {
+                //General Oops
                 tbResult.Text = exception.ToString();
             }
         }
